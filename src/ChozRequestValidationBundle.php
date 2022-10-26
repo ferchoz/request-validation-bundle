c6ca25378f96d91bb022d24356591a07c489cd47
Fernando Rivas <ferchunet@gmail.com>
2022-10-26 10:13:13 +0000

    feat: use different configuration for bundle

--- src/ChozRequestValidationBundle.php
+++ src/ChozRequestValidationBundle.php
@@ -2,8 +2,8 @@
 
 namespace Choz\RequestValidationBundle;
 
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ChozRequestValidationBundle extends AbstractBundle
+class ChozRequestValidationBundle extends Bundle
 {
 }
